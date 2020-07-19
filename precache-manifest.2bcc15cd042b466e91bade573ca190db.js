d8fe8b336e1e60e5da8bd58ce68cbd2209e0356d
Priyanka Jalan <prijalan2001@gmail.com>
2020-07-19 12:31:01 +0530

    Updates

---
+++ precache-manifest.2bcc15cd042b466e91bade573ca190db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5feeedd170bf90d265e597de5a5b5bde",
+    "url": "/myriad-frontend/index.html"
+  },
+  {
+    "revision": "8e71a9584565200b7820",
+    "url": "/myriad-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "69a57dd919e46c353896",
+    "url": "/myriad-frontend/static/css/main.f5e9fd5a.chunk.css"
+  },
+  {
+    "revision": "8e71a9584565200b7820",
+    "url": "/myriad-frontend/static/js/2.715839b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myriad-frontend/static/js/2.715839b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a57dd919e46c353896",
+    "url": "/myriad-frontend/static/js/main.578075e5.chunk.js"
+  },
+  {
+    "revision": "316d1306693e2dda409c",
+    "url": "/myriad-frontend/static/js/runtime-main.80537a3d.js"
+  }
+]);
